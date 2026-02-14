b94e6eed1c4eb29be2735c124afa8f9868952ca9
erdebaru <46704334+erdebaru@users.noreply.github.com>
2026-02-14 05:24:02 +0000

    refactor: remove Redux dependencies and implement custom store context for state management

---
+++ src/client/app/components/User.tsx
@@ -0,0 +1,14 @@
+import { useStore } from "../store"
+
+interface UserProps {
+    
+}
+
+export default function User({}: UserProps){
+    const context = useStore();
+    return (
+        <>
+    
+        </>
+    )
+}
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App';
-import store from './app/store'
-import { Provider } from 'react-redux'
 
 const root = document.getElementById('root');
 if(root){
     ReactDOM.createRoot(root).render(
         <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
+            <App />
         </React.StrictMode>,
     )
 }
